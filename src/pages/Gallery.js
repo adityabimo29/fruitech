b56bd05dc2a97ff8e3002006dda3067138b4680f
Pirman Saputra <pirman0290@gmail.com>
2020-02-03 14:50:56 +0700

    Add Menu gallery

---
+++ src/pages/Gallery.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import 'antd/dist/antd.css';
+import { Row, Col } from 'antd';
+
+export default function Gallery() {
+    return (
+        <div>
+            <Row>
+                <Col span={12}>
+                    <h1>GRID GALLERY</h1>
+                </Col>
+            </Row>
+        </div>
+    )
+}
