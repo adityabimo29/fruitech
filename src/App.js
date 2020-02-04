133e117c8018256f13c10784e586657da5b990ad
Frans Jesky <fransjesky@hotmail.com>
2020-02-04 16:08:29 +0700

    Created About Page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import OurTeam from "./pages/OurTeam";
+import About from "./pages/About";
 
 function App() {
   return (
     <div className="App">
-      <OurTeam />
+      {/* <OurTeam /> */}
+      <About />
     </div>
   );
 }
